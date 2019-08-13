63958ac72795f04c38ffab62d911850b2442b428
John Cokos <highlander.44@gmail.com>
2019-08-13 10:24:34 -0700

    Merry Christmas ...

--- scripts/script.js
+++ scripts/script.js
@@ -2,106 +2,82 @@
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
-// var pikePlace = {
-//     name: '1st and Pike',
-//     minCust : 23,
-//     maxCust : 65,
-//     avgCookie : 6.3,
-//     random : function () {
-//         return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust; //The maximum is inclusive and the minimum is inclusive
-//     },
-
-//     };
+var locations = [];
 
 var seaTac = {
   name: 'SeaTac Airport',
-  cookies : [],
-  minCust : 3,
-  maxCust : 24,
-  avgCookie : 1.2,
-  random : function () {
-    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust; //The maximum is inclusive and the minimum is inclusive
+  cookies: [],
+  minCust: 3,
+  maxCust: 24,
+  avgCookie: 1.2,
+  random: function () {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust; // Source: MDN Random Docs/Example
   },
-  cookieMultiply : function () {
+  generateProjection: function () {
     for (var i = 0; i < hours.length; i++) {
-      // var = Math.round(Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust);
-      var multiply = this.random() * this.avgCookie;
+      var multiply = Math.ceil(this.random() * this.avgCookie);
       this.cookies.push(multiply);
-      // this.cookies.push(multiply);
     }
-    var elementList = document.getElementById('list)');
-    for(var ii = 0; ii < seaTac.cookies.lenght; ii++) {
-      var newListItem = document.createElement('li');
-      newListItem.textContent = seaTac.cookies[i];
-      elementList.appendChild(newListItem);
-    }
-
-
   }
 };
 
-// var seaTac = {
-//   cookies : [],
-//   name: 'SeaTac Airport',
-//   minCust : 3,
-//   maxCust : 24,
-//   avgCookie : 1.2,
-//   random : function () {
-//     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust; //The maximum is inclusive and the minimum is inclusive
-//   },
-//   cookieMultiply : function () {
-//     for (var i = 0; i < hours.length; i++) {
-//       // var = Math.round(Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust);
-//       var multiply = this.random() * this.avgCookie;
-//       this.cookies.push(multiply);
-//       // this.cookies.push(multiply);
-//       return ;
-//     }
-//     var elementList = document.getElementById('list)');
-//     for(var ii = 0; ii < seaTac.cookies.lenght; ii++) {
-//       var newListItem = document.createElement('li');
-//       newListItem.textContent = seaTac.cookies[i];
-//       elementList.appendChild(newListItem);
-//     }
 
-//   },
-
-// };
-// console.log(cookieMultiply);
-// console.log(multiply);
-console.log(seaTac);
-console.log(seaTac.random());
-console.log(seaTac.cookies);
+var johnsHouse = {
+  name: 'Johns House',
+  cookies: [],
+  minCust: 4,
+  maxCust: 47,
+  avgCookie: 5.6,
+  random: function () {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust; // Source: MDN Random Docs/Example
+  },
+  generateProjection: function () {
+    for (var i = 0; i < hours.length; i++) {
+      var multiply = Math.ceil(this.random() * this.avgCookie);
+      this.cookies.push(multiply);
+    }
+  }
+};
 
 
-// var seaCenter = {
-//     name: 'Seattle Center',
-//     minCust : 11,
-//     maxCust : 38,
-//     avgCookie : 3.7,
-//     };
+function render() {
+  // Find the container (section with an id="locations")
+  // Loop over the locations
+  // <h2> with it's name
+  // <ul>
+  //   Loop over the [hours] and and make an <li> with the hour and the projection from location [cookies]
 
-// var capHill = {
-//     name: 'Capitol Hill',
-//     minCust : 20,
-//     maxCust : 38,
-//     avgCookie : 2.3,
-//     };
+  var section = document.getElementById('locations');
 
-// var alki = {
-//     name: 'Alki Beach',
-//     minCust : 2,
-//     maxCust : 16,
-//     avgCookie : 4.6,
-//     };
+  for (var i = 0; i < locations.length; i++) {
 
-// var title = document.getElementById('pageName');
-// title.textContent = enrique.name;
+    var location = locations[i];
 
+    var title = document.createElement('h3');
+    title.textContent = location.name;
+    section.appendChild(title);
 
-//find the ul
-//For enrique.children.lenght
-//create an li
-//set the textContent to the name of the name of the child
-//append li as a child to ul
+    var list = document.createElement('ul');
+
+    for (var j = 0; j < hours.length; j++) {
+      var projection = hours[j] + ': ' + location.cookies[j];
+      var li = document.createElement('li');
+      li.textContent = projection;
+      list.appendChild(li);
+    }
+
+    section.appendChild(list);
+  }
+}
+
+function initialize() {
+  seaTac.generateProjection();
+  johnsHouse.generateProjection();
+
+  locations.push(seaTac);
+  locations.push(johnsHouse);
+}
+
+initialize();
+render();
 
