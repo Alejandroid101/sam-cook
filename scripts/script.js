7a220bcde59440461ac564f837c9352bd1022c44
ENRIQUE RIVERA <KIK.RDL3@GMAIL.COM>
2019-08-14 08:14:56 -0700

    kind of works, still no actual table

--- scripts/script.js
+++ scripts/script.js
@@ -6,65 +6,91 @@ var listOfLocations = [];
 
 
 function Locations(name, minCust, maxCust, avgCookie){
-  this.dataOne = name;
-  this.dataTwo = minCust;
+  this.name = name;
+  this.minCust = minCust;
   this.dataThree = maxCust;
   this.dataFour = avgCookie;
-
   listOfLocations.push(this);
+
 }
 
-var firstPike	= new Locations('1st And Pike', '23',	'65',	'6.3');
-var seaTac = new Locations('SeaTac Airport', '3',	'24',	'1.2');
-var seaCenter = new Locations('Seattle Center', '11',	'38',	'3.7');
-var capHill = new Locations('Capito Hill', '20',	'38',	'2.3');
-var alki = new Locations('Alki Beach', '2',	'16',	'4.6');
+Locations.prototype.random = function () {
+  return Math.floor(Math.random() * (this.dataThree - this.minCust + 1)) + this.minCust;
+};
+Locations.prototype.generateProjection = function () {
+  var cookies = [];
+  for (var i = 0; i < hours.length; i++) {
+    var multiply = Math.floor(this.random() * this.dataFour);
+    cookies.push(multiply);
+  }
+  return cookies;
+};
+
+
+var firstPike	= new Locations('1st And Pike', 23,	65,	6.3);
+var seaTac = new Locations('SeaTac Airport', 3,	24,	1.2);
+var seaCenter = new Locations('Seattle Center', 11,	38,	3.7);
+var capHill = new Locations('Capito Hill', 20,	38,	2.3);
+var alki = new Locations('Alki Beach', 2,	16,	4.6);
+
+console.log(firstPike.generateProjection());
+console.log(seaTac.generateProjection());
+console.log(seaCenter.generateProjection());
+console.log(capHill.generateProjection());
+console.log(alki.generateProjection());
+
 
 for (var i = 0; i <listOfLocations.length; i++) {
   console.log(listOfLocations[i]);
-  generateProjection = function () {
-    for (var i = 0; i < hours.length; i++) {
-      var multiply = Math.ceil(this.random() * this.avgCookie);
-      this.cookies.push(multiply);
-    }
-    random = function () {
-      return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
-    };
-  };
+}
+function addRow(tableID) {
+  // Get a reference to the table
+  let tableRef = document.getElementById(tableID);
+
+  // Insert a row at the end of the table
+  let newRow = tableRef.insertRow(-1);
+
+  // Insert a cell in the row at index 0
+  let newCell = newRow.insertCell(0);
+
+  // Append a text node to the cell
+  let newText = document.createTextNode('New bottom row');
+  newCell.appendChild(newText);
 }
 
+// Call addRow() with the table's ID
+addRow('my-table');
 
-function render() {
 
-  var section = document.getElementById('Locations');
+// function render() {
 
-  for (var i = 0; i < listOfLocations.length; i++) {
+//   var section = document.getElementById('Locations');
 
-    var location = listOfLocations[i];
+//   var specific_tbody = document.getElementById(tbody_id);
+//   let row = specific_tbody.insertRow(index)
 
-    var title = document.createElement('h3');
-    title.textContent = location.name;
-    section.appendChild(title);
+//   for (var i = 0; i < listOfLocations.length; i++) {
 
-    var list = document.createElement('ul');
+//     var location = listOfLocations[i];
 
-    for (var j = 0; j < hours.length; j++) {
-      var projection = hours[j] + ': ' + location.cookies[j];
-      var li = document.createElement('li');
-      li.textContent = projection;
-      list.appendChild(li);
-    }
+//     var title = document.createElement('h3');
+//     title.textContent = location.name;
+//     section.appendChild(title);
 
-    section.appendChild(list);
-  }
-}
+//     var list = document.createElement('ul');
+
+//     for (var j = 0; j < hours.length; j++) {
+//       var projection = hours[j] + ': ' + cookies[j];
+//       var li = document.createElement('li');
+//       li.textContent = projection;
+//       list.appendChild(li);
+//     }
+
+//     section.appendChild(list);
+//   }
+// }
 
 function initialize() {
-  firstPike.generateProjection();
-  seaTac.generateProjection();
-  seaCenter.generateProjection();
-  capHill.generateProjection();
-  alki.generateProjection();
 
   listOfLocations.push(firstPike);
   listOfLocations.push(seaTac);
@@ -75,7 +101,18 @@ function initialize() {
 }
 
 initialize();
-render();
+// render();
+
+
+
+
+
+
+
+
+
+
+
 
 
 
